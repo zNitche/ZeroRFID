468eebeb5569f903a9a8ad31c719b9b9687db09d
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2021-10-24 18:27:19 +0200

    changed rc522 library for better low level operations

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 import argparse
 import RPi.GPIO as GPIO
-from mfrc522 import SimpleMFRC522
+from pirc522 import RFID
+import time
 
 
 def read_rfid(reader):
-    id, text = reader.read()
+    util = reader.util()
 
-    return id, text
+    while True:
+        # Wait for tag
+        reader.wait_for_tag()
+
+        # Request tag
+        (error, data) = reader.request()
+        if not error:
+            print("\nDetected")
+
+            (error, uid) = reader.anticoll()
+            if not error:
+                # Print UID
+                print("Card read UID: " + str(uid[0]) + "," + str(uid[1]) + "," + str(uid[2]) + "," + str(uid[3]))
+
+                util.set_tag(uid)
+                util.auth(reader.auth_b, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF])
+
+                util.dump()
+                util.deauth()
+
+                break
+
+        time.sleep(1)
 
 
 def write_rfid(reader, text):
@@ -15,35 +38,36 @@ def write_rfid(reader, text):
 
 def main(opt):
     mode = opt.mode[0]
-    reader = SimpleMFRC522()
+
+    GPIO.cleanup()
+
+    reader = RFID()
 
     if mode == "c":
-        print("Copying RFID card, waiting for first card...")
-
-        id, text = read_rfid(reader)
-
-        print("Card read, waiting for second card...")
-
-        write_rfid(reader, text)
-
+        # print("Copying RFID card, waiting for first card...")
+        #
+        # id, text = read_rfid(reader)
+        #
+        # print("Card read, waiting for second card...")
+        #
+        # write_rfid(reader, text)
+        #
         print("Done...")
 
     elif mode == "r":
         print("Reading RFID Card, waiting for card...")
 
-        id, text = read_rfid(reader)
-
-        print(f"ID: {id}")
-        print(f"Content: {text}")
+        read_rfid(reader)
 
     elif mode == "w":
-        text = input("Content > ")
-
-        print("Waiting for RFID tag...")
-
-        write_rfid(reader, text)
-
-        print("Done...")
+        # text = input("Content > ")
+        #
+        # print("Waiting for RFID tag...")
+        #
+        # write_rfid(reader, text)
+        #
+        # print("Done...")
+        pass
 
     else:
         print("Unsupported mode...")
